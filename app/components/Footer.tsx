2b2fabde137c06e98e4b273baa133d5fb4681fc3
s.saramud <s.saramud@3itech.ru>
2024-11-05 11:28:36 +0300

    test

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer>
       <p>
-        &copy; {new Date().getFullYear()} diMaxcar автоподбор. Все права
+        &copy; {new Date().getFullYear()} diMaxвввввввcar автоподбор. Все права
         защищены.
       </p>
     </footer>
